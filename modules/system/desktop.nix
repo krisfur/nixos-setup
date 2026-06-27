720f8ab1983b7b82ec7c360788c8f6e7cf5b0cab
Krzysztof Furman <k_furman@outlook.com>
2026-06-27 13:14:46 +0100

    they renamed the font ting

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -84,7 +84,7 @@ in
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
     noto-fonts
-    noto-fonts-emoji
+    noto-fonts-color-emoji
   ];
   fonts.fontconfig.defaultFonts.monospace = [ "JetBrainsMono Nerd Font" ];
 }
